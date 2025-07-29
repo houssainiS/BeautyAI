94667256adcbb4ae77f7f13c2268afa7b8212ef7
houssainiS <houssaini.slimen@gmail.com>
2025-07-29 22:55:11 +0100

    testing /health response for render

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.http import HttpResponse
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('upload/', views.upload_photo, name='upload_photo'),  
+    path('upload/', views.upload_photo, name='upload_photo'),
+    path('health/', lambda request: HttpResponse('ok')),
 ]
