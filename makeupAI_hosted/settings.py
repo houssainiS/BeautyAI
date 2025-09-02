beee553ff840917f9771ead507613ef8375efc5a
houssainiS <houssaini.slimen@gmail.com>
2025-09-02 21:04:26 +0100

    updated settings , test 6

--- makeupAI_hosted/settings.py
+++ makeupAI_hosted/settings.py
@@ -110,3 +110,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ###
 CORS_ALLOWED_ORIGINS = []
 CORS_ALLOW_CREDENTIALS = True
+
+
+### because shopify webhook
+
+X_FRAME_OPTIONS = "ALLOWALL"
+ 
