48be0bf672ac698c6b04fb4bc101eeb1417df006
houssainiS <houssaini.slimen@gmail.com>
2025-09-02 19:50:16 +0100

    fixed uninstall webhook is inside webhooks file not views.py , second test

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from . import views , webhooks
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('upload/', views.upload_photo, name='upload_photo'),  
     path('submit-feedback/', views.submit_feedback, name='submit_feedback'),
     path("auth/callback/", views.oauth_callback, name="oauth_callback"),
-    path("webhooks/app_uninstalled/", views.app_uninstalled, name="app_uninstalled"),
+    path("webhooks/app_uninstalled/", webhooks.app_uninstalled, name="app_uninstalled"),
 ]
